9d421e3bcce09bfd73487ded36765119a39c00b4
jjfarlinger <jjfarlinger@gmail.com>
2015-10-26 21:47:24 +0000

    Added commenting code

--- ex1.rb
+++ ex1.rb
@@ -5,3 +5,19 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
+
+#Study Drills:
+# 1. Make your script print another line.
+puts "You just learned how to print a Ruby line of code."
+#2. Make your script print only one of the lines. 
+puts "Make your script print only one of the lines from above."
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+puts "You just learned how to print a Ruby line of code."
+# 3. What does the # do? 
+#    It comments out that particular code following the '#'
