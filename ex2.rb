e6a5369cc6732baf44d70fbbb572908222275c5a
jjfarlinger <jjfarlinger@gmail.com>
2015-10-26 21:51:52 +0000

    added commenting code

--- ex2.rb
+++ ex2.rb
@@ -7,3 +7,9 @@ puts "I could have code like this." # and the comment after is ignored
 # puts "This won't run."
 
 puts "This will run."
+
+# Study Drills
+# 1. Find out if you were right about what the # character does. Yes
+# 2-4. Take your ex2.rb file and review each line going backward. Start at the last line, 
+#      and check each word in reverse against what you should have typed. Fix any error you find.
+#      Now read aloud including saying each character. If more errors found, fix them.   
