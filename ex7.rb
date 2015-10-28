dc5c11a99afce2e3b580a6cdb8b343ad88c926ca
jjfarlinger <jjfarlinger@gmail.com>
2015-10-28 01:42:17 +0000

    Added code and commenting and proofed to complete this exercise

---
+++ ex7.rb
@@ -0,0 +1,33 @@
+#displays string
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 # what does this do? It prints the dot character (.) 10 times.
+# declare variables end1 - end12 spelling CheeseBurger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line
+# what does it do?
+# by using print and puts, CheeseBurger displays on one line
+# If you use puts on both lines, 'Cheese' appears on one line and 'Burger appears on the next line.'
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# Study Drills
+# 1. Go back through and write a comment on what each line does.
+# 2. Read each one backward or out loud to find your errors.
+# 3. From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# 4. When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# 5. Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
+
