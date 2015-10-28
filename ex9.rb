0166ac4f971eed0c6b5deba921808b7d660f297b
jjfarlinger <jjfarlinger@gmail.com>
2015-10-28 02:12:08 +0000

    Completed code, comments and code proofed.

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+#Create variables days and months
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#Displays "Here are the days: Mon Tue Wed Thu Fri Sat Sun"
+puts "Here are the days: #{days}"
+#Displays "Here are the months: <Lists each month on a new line>"
+puts "Here are the months: #{months}"
+#Allows you to put multiple-line string in between the """ Add multiple line string code here ""
+puts """  
+What do the three double-quotes do?
+With the three double-quotes,
+we'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
