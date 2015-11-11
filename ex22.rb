e551faa7eceac0afeae6b501b46ea877413d77ff
jjfarlinger <jjfarlinger@gmail.com>
2015-11-11 20:12:37 +0000

    List of key words and charachters

---
+++ ex22.rb
@@ -0,0 +1,54 @@
+#Exercise 22: What do You Know So far. Make a list of the words and symbols you have used thusfar.
+#Write what that word/symbol does next to it.
+
+#     puts = prints a string to screen and create new line.
+#     # octothorpe is used to make comments. Anything typed after # on the line is ignored.
+#     + plus (addition)
+#     + Also used for string concatenation.
+#     - minus (subtraction)
+#     / slash (divide)
+#     * asterisk (multiply)
+#     % percent (mod operator)
+#     <less-than
+#     greater-than
+#     <= less-than-equal
+#     >= greater-than-equal
+#     #{var} used to populate variable values into a string.
+#     variable - used to store values
+#     = assignment operator - assigns value Ex: myVar = 35. myVar now holds value of 35.
+#     print = prints to screen but does not add a carriage return at the end like puts
+#     \n adds a new line/carriage return.
+#     %{var} Used when you want to apply the same format to multiple values.
+#     """ Multi line string """
+#     \ backslash is used to escape. Used to escape symbols.
+#     \t tab
+#     \\ Backslash ()
+#     \' Single-quote (')
+#     \" double-quote (")
+#     \a bell (BEL)
+#     \b backspace (BS)
+#     \f formfeed (FF)
+#     \r Carriage Return (CR)
+#     \uxxxx	Character with 16-bit hex value xxxx (Unicode only)
+#     \v	vertical tab (VT)
+#     \ooo	Character with octal value ooo
+#     \xhh	Character with hex value hh
+#     gets.chomp = Use to get user input (gets) and removes the newline (chomps)
+#     gets.chomp.to_i Takes user input and converts to an integer
+#     .to_f converts to float
+#     ARGV The argument variable that holds the arguments you pass when you run the script.
+#     $stdin input from the user given usually stored in a variable.
+#     open command to open a file
+#     read command to read/print the file
+#     close command to close the file
+#     seek command to locate a particular place (in bytes) in a file.
+#     readline Reads just one line of a text file
+#     truncate Empties the file. 
+#     write('stuff') Writes (whatever is inside parenthesis) to the file.
+#     File.exist? Checks to see if file exists
+#     functions - Functions do three things: they name pieces of code, take arguments, and make mini-scripts
+#     def - Defines a function (def hello(arg1, arg2))
+#     *args is a lot like ARGV paramenter, but for functions def hello(*args) \n arg1, arg2 = args
+#     Above not needed can do:  hello(arg1, arg2) which eliminates the second line above.
+#     return Sets variables to be a value from a function
+
